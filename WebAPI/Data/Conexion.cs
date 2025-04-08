730a7199d5032a682e868b168e28bc6d89262192
AzureAD\MartinMartinezCasano <martin.martinez@fusionit.uy>
2025-04-07 21:50:49 -0300

    MM - Deploy azure settings

--- WebAPI/Data/Conexion.cs
+++ WebAPI/Data/Conexion.cs
@@ -7,7 +7,7 @@ namespace WebAPI.Data
 {
     public class Conexion
     {
-        public static string RutaConexion = "Data Source=.;Initial Catalog=rpmtecno;Integrated Security=True";
-        //public static string RutaConexion = "Server=tcp:db-server-rpm-tecno-dev.database.windows.net,1433;Initial Catalog=db-rpm-tecno;Persist Security Info=False;User ID=admin-rpm-tecno;Password=Password.01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //public static string RutaConexion = "Data Source=.;Initial Catalog=rpmtecno;Integrated Security=True";
+        public static string RutaConexion = "Server=tcp:rpm-tecno-server.database.windows.net,1433;Initial Catalog=rpm-tecno-db;Persist Security Info=False;User ID=rpm-admin;Password=LaMismaDeSiempre.1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
