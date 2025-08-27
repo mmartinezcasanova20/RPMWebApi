edb098bae36d4b990ac3c28c2a53bdd6506dea73
AzureAD\MartinMartinezCasano <martin.martinez@fusionit.uy>
2025-08-27 16:10:17 -0300

    MM - Fixes Alta Orden

--- WebAPI/Models/Servicio.cs
+++ WebAPI/Models/Servicio.cs
@@ -4,14 +4,14 @@
     {
         public int NumeroOrden { get; set; }
         public int CICliente { get; set; }
-        public string NombreCliente { get; set; }
-        public string Telefono { get; set; }
+        public string? NombreCliente { get; set; }
+        public string? Telefono { get; set; }
         public string Modelo { get; set; }
         public string TipoEquipo { get; set; }
         public int TipoServicio { get; set; }
         public string TrabajoARealizar { get; set; }
         public int Tecnico { get; set; }
-        public string NombreTecnico { get; set; }
+        public string? NombreTecnico { get; set; }
         public int PrecioReparacion { get; set; }
         public string FechaRecibido { get; set; }
         public string FechaFinalizado { get; set; }
