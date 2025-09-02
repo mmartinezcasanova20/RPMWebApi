8651f26c7be6fc48bd74d17376905691f8aa1547
AzureAD\MartinMartinezCasano <martin.martinez@fusionit.uy>
2025-09-01 21:04:03 -0300

    MM - actualización de estado de reparación

--- WebAPI/Models/Servicio.cs
+++ WebAPI/Models/Servicio.cs
@@ -6,16 +6,16 @@
         public int CICliente { get; set; }
         public string? NombreCliente { get; set; }
         public string? Telefono { get; set; }
-        public string Modelo { get; set; }
-        public string TipoEquipo { get; set; }
+        public string? Modelo { get; set; }
+        public string? TipoEquipo { get; set; }
         public int TipoServicio { get; set; }
-        public string TrabajoARealizar { get; set; }
+        public string? TrabajoARealizar { get; set; }
         public int Tecnico { get; set; }
         public string? NombreTecnico { get; set; }
         public int PrecioReparacion { get; set; }
-        public string FechaRecibido { get; set; }
+        public string? FechaRecibido { get; set; }
         public string FechaFinalizado { get; set; }
-        public string Nota { get; set; }
+        public string? Nota { get; set; }
         public  int IdEstado { get; set; }
         public int Borrado { get; set; }
         public int NumeroOrdenVinculada { get; set; }
